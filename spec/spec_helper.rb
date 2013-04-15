828a56a2f3cd314fa27bb2738a59f908d670a025
David Conner <dconner.pro@gmail.com>
2013-04-15 07:33:09 -0600

    Spec Helper: refactored

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,6 @@
-require 'minitest/autorun'
-require 'minitest/matchers'
-require "pry"
-
 require "pundit"
 require "active_model/naming"
-
-require 'shoulda/matchers/pundit/minitest'
-
-if ENV['MINITESTREPORTER']
-  require "minitest/reporters"
-  require "minitest/reporters/ws"
-
-  reporter = case ENV['MINITESTREPORTER']
-             when 'ws' then MiniTest::Reporters::Ws::Reporter.new
-             when 'spec' then MiniTest::Reporters::SpecReporter.new
-             else MiniTest::Reporters::ProgressReporter.new
-  end
-
-  Minitest::Reporters.use! reporter
-end
+require "pry"
 
 class ArticlePolicy < Struct.new(:user, :record)
 
